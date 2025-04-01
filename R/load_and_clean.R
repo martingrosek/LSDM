3a2cb813f7536f9b0e7042d123feab7d2cea8587
martingrosek <martin.grosek@student.um.si>
2025-04-01 21:05:32 +0100

    removed Std_confidence, -Var_confidence

--- R/load_and_clean.R
+++ R/load_and_clean.R
@@ -23,3 +23,18 @@ summary(landclass)
 
 
 head(wildfires)
+
+
+# Preverjanje manjkajočih vrednosti
+colSums(is.na(wildfires))
+colSums(is.na(weather))
+colSums(is.na(forecasts))
+colSums(is.na(ndvi))
+colSums(is.na(landclass))
+
+
+#we removed these two columns ->too much missing values -> wont use it with analyzing fires therefore, their presence would only unnecessarily complicate further data processing and modeling. 
+wildfires <- wildfires %>% select(-Std_confidence, -Var_confidence)
+
+
+
